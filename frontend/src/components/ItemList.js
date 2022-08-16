b8ce39b90e9f4db5e74064eb62d4109f846aef66
nofearOnline <navonor@gmail.com>
2022-08-17 01:16:55 +0300

    linting

--- frontend/src/components/ItemList.js
+++ frontend/src/components/ItemList.js
@@ -8,13 +8,21 @@ const ItemList = (props) => {
   }
 
   if (props.items.length === 0) {
-    if (props.title == ""){
+    if (props.title == "") {
       return <div className="py-4 no-items">No items are here... yet.</div>;
     }
-    return <div className="py-4 no-items text-center" style={{backgroundColor: "rgba(255,255,255,0.1)"}}>
-      <i className="bi bi-emoji-frown-fill text-white" style={{ fontSize: "36px"}} ></i>
-      <div id="empty">No items found for "{props.title}"</div>
-      </div>;
+    return (
+      <div
+        className="py-4 no-items text-center"
+        style={{ backgroundColor: "rgba(255,255,255,0.1)" }}
+      >
+        <i
+          className="bi bi-emoji-frown-fill text-white"
+          style={{ fontSize: "36px" }}
+        ></i>
+        <div id="empty">No items found for "{props.title}"</div>
+      </div>
+    );
   }
 
   return (
