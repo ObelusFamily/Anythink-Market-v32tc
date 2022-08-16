a7c9928dbf8704e61d23551f0254818775b20312
nofearOnline <navonor@gmail.com>
2022-08-17 01:34:51 +0300

    Making search a secert

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -1,15 +1,17 @@
 import React from "react";
+import { useState } from "react";
 import logo from "../../imgs/logo.png";
 import SearchBar from "./SearchBar";
 
 const Banner = () => {
+  let [getPressed,setGetPressed] = useState(false)
   return (
     <div className="banner text-white">
       <div className="container p-4 text-center">
         <img src={logo} alt="banner" />
         <div style={{ fontSize: "20px" }}>
-          <span id="get-part">A place to get</span> &nbsp;
-          <SearchBar />
+          <span id="get-part">A place to <span onClick={() => {setGetPressed(true)}}>get</span></span>
+          <SearchBar show={getPressed}/>
           <span> the cool stuff.</span>
         </div>
       </div>
