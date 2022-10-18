21dfb3db80723bc8e1af537e96effe1260600019
or-navon <or@cypago.com>
2022-10-18 17:58:29 +0300

    Moving the newrelic script to the procfile itself

--- backend/scripts/heroku_release.sh
+++ backend/scripts/heroku_release.sh
@@ -14,5 +14,3 @@ alembic upgrade head
 printf "\n⏳${YELLOW}   [Release Phase]: Seeding.${NO_COLOR}\n"
 ./seeds.sh
 printf "\n🎉${GREEN}   [Release Phase]: Database is up to date.${NO_COLOR}\n"
-
-NEW_RELIC_CONFIG_FILE=newrelic.ini newrelic-admin run-program 
